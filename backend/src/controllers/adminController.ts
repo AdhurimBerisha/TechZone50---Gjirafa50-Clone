371db9780b6225183251433522b1d5ad33f1ccae
AdhurimBerisha <adhurimberisha07@gmail.com>
2026-04-11 17:18:15 +0200

    Basic Route and Controller Setup

---
+++ backend/src/controllers/adminController.ts
@@ -0,0 +1,17 @@
+const getAdminDashboard = async (req: Request, res: Response) => {};
+
+const getAdminUsers = async (req: Request, res: Response) => {};
+
+const getAdminProducts = async (req: Request, res: Response) => {};
+
+const getAdminOrders = async (req: Request, res: Response) => {};
+
+const getAdminSettings = async (req: Request, res: Response) => {};
+
+export {
+  getAdminDashboard,
+  getAdminUsers,
+  getAdminProducts,
+  getAdminOrders,
+  getAdminSettings,
+};
---
+++ backend/src/controllers/userController.ts
@@ -0,0 +1,11 @@
+import type { Request, Response } from "express";
+
+export const createUser = async (req: Request, res: Response) => {};
+
+export const getUsers = async (req: Request, res: Response) => {};
+
+export const updateUser = async (req: Request, res: Response) => {};
+
+export const deleteUser = async (req: Request, res: Response) => {};
+
+export const getUserById = async (req: Request, res: Response) => {};
