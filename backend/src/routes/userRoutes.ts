e683dff93dad0bd277495ce655a990c91a43dc74
AdhurimBerisha <adhurimberisha07@gmail.com>
2026-04-18 00:50:02 +0200

    Changes

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -2,8 +2,6 @@ import { Router } from "express";
 import {
   syncUser,
   updateProfile,
-  createUser,
-  getUsers,
   updateUser,
   deleteUser,
   getUserById,
@@ -13,9 +11,6 @@ const router = Router();
 
 router.post("/sync", syncUser);
 router.put("/profile", updateProfile);
-router.post("/create", createUser);
-
-router.get("/", getUsers);
 router.get("/:id", getUserById);
 router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
